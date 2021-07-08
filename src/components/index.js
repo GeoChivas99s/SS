69806e760498feabb89a4fbb9e100b139812fe91
Geovane Chivas <Chivasc95@gmail.com>
2021-07-08 23:10:41 +0100

    fix: bugs

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as About } from './About';
+export { default as About } from './about';
 export { default as Layout } from './layout';
-export { default as Header } from './Header';
+export { default as Header } from './header';
 export { default as Signature } from './signature';
 export { default as SignatureDev } from './signature-dev';
 export { default as SignatureEmp} from './signature-emp';
